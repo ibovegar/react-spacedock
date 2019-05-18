5e40e0f1df4c62fad26c64478187583948d3a09d
Vegar Eeg <vegareeg@gmail.com>
2019-05-18 17:42:03 +0200

    add API_URL based on environment

--- src/api/spaceship.api.ts
+++ src/api/spaceship.api.ts
@@ -1,19 +1,15 @@
-// import axios from 'axios';
 import axios, { AxiosResponse } from 'axios';
 import { ISpaceship } from 'models';
-// import ISpaceship from 'models/spaceship.model';
-// import { environment } from '../../environments/environment';
-// (`${environment.apiUrl}/movies`)
+
+const url = process.env.REACT_APP_API_URL;
+
+export const getAll = async (): Promise<ISpaceship[]> => {
+  const response: AxiosResponse = await axios.get(`${url}/spaceships`);
+  return response.data;
+};
 
 // export const getAll = (): Promise<ISpaceship[]> => {
 //   return axios
 //     .get('http://localhost:1337/spaceships')
 //     .then((response: AxiosResponse) => response.data);
 // };
-
-export const getAll = async (): Promise<ISpaceship[]> => {
-  const response: AxiosResponse = await axios.get(
-    'http://localhost:1337/spaceships',
-  );
-  return response.data;
-};
