d032bab6b09f4b301bf746ffc68634c1aa2861d1
Vegar Eeg <vegareeg@gmail.com>
2019-05-20 19:36:07 +0200

    remove log

--- src/pages/Engineering/engineering.page.tsx
+++ src/pages/Engineering/engineering.page.tsx
@@ -8,8 +8,6 @@ import { Spaceship } from 'containers';
 // }
 
 const Engineering = ({ match }: RouteComponentProps<any>) => {
-  console.log(match);
-
   // Display list of ships. Select a ship to show engineering section (below ship list). Ship list is animated as slide to height 100px. Easily accesible if you eant to change ship
 
   return (
