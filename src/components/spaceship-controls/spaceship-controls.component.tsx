58b17670ce4c3ef7e97c8972eb5ae7b098b1843d
Vegar Eeg <vegareeg@gmail.com>
2019-05-25 19:47:19 +0200

    add setUpgrade event

--- src/components/spaceship-controls/spaceship-controls.component.tsx
+++ src/components/spaceship-controls/spaceship-controls.component.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
-import { ISpaceship, IAvailableUpgrades } from 'models';
+import { ISpaceship, IAvailableUpgrades, IUpgrade } from 'models';
 import SpaceshipControl from './spaceship-control/spaceship-control.component';
 
 interface IProps {
   spacecraft: ISpaceship;
   upgrades: IAvailableUpgrades;
+  onSelectUpgrade: (upgrade: IUpgrade) => void;
 }
 
-const SpaceshipControls: React.FC<IProps> = ({ spacecraft, upgrades }) => {
+const SpaceshipControls: React.FC<IProps> = props => {
+  const { spacecraft, upgrades, onSelectUpgrade } = props;
+
   const {
     deflector,
     engine,
@@ -22,26 +25,31 @@ const SpaceshipControls: React.FC<IProps> = ({ spacecraft, upgrades }) => {
   return (
     <>
       <SpaceshipControl
+        onSelectUpgrade={upgrade => onSelectUpgrade(upgrade)}
         type="Deflector"
         selectedUpgrade={deflector}
         selectableUpgrades={upgrades.deflector}
       />
       <SpaceshipControl
+        onSelectUpgrade={upgrade => onSelectUpgrade(upgrade)}
         type="Engine"
         selectedUpgrade={engine}
         selectableUpgrades={upgrades.engine}
       />
       <SpaceshipControl
+        onSelectUpgrade={upgrade => onSelectUpgrade(upgrade)}
         type="Plating"
         selectedUpgrade={plating}
         selectableUpgrades={upgrades.plating}
       />
       <SpaceshipControl
+        onSelectUpgrade={upgrade => onSelectUpgrade(upgrade)}
         type="Stabilizer"
         selectedUpgrade={stabilizer}
         selectableUpgrades={upgrades.stabilizer}
       />
       <SpaceshipControl
+        onSelectUpgrade={upgrade => onSelectUpgrade(upgrade)}
         type="Weapons"
         selectedUpgrade={weapons}
         selectableUpgrades={upgrades.weapons}
--- src/containers/spaceship-builder/spaceship-builder.container.tsx
+++ src/containers/spaceship-builder/spaceship-builder.container.tsx
@@ -5,6 +5,7 @@ import { isEmpty } from 'utils/helpers';
 import Box from '@material-ui/core/Box';
 import * as interfaces from './spaceship-builder.interface';
 import { Typography } from '@material-ui/core';
+import { IUpgrade } from 'models';
 
 class SpaceshipBuilder extends React.Component<interfaces.IProps, {}> {
   render() {
@@ -13,6 +14,11 @@ class SpaceshipBuilder extends React.Component<interfaces.IProps, {}> {
     if (isEmpty(spacecraft)) {
       return <div>Loading spacecraft</div>;
     }
+
+    const handleSelectUpgrade = (upgrade: IUpgrade) => {
+      // spacecraftstore.setActiveUpgrade(spacecraft, upgrade)
+      console.log(upgrade);
+    };
 
     return (
       <Box display="flex">
@@ -26,7 +32,11 @@ class SpaceshipBuilder extends React.Component<interfaces.IProps, {}> {
           <Typography variant="h4" gutterBottom>
             Build Controls
           </Typography>
-          <SpaceshipControls spacecraft={spacecraft} upgrades={upgrades} />
+          <SpaceshipControls
+            spacecraft={spacecraft}
+            upgrades={upgrades}
+            onSelectUpgrade={upgrade => handleSelectUpgrade(upgrade)}
+          />
         </Box>
         <Box width={500} m={2}>
           <Typography variant="h4" gutterBottom>
